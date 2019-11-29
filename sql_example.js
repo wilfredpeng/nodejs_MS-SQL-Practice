b1e81387d4c96afb25d26bd63de37cfd92efeee1
wilfredpeng <35424319+wilfredpeng@users.noreply.github.com>
2019-11-29 11:21:10 +0800

    Update sql_example.js

--- sql_example.js
+++ sql_example.js
@@ -11,7 +11,7 @@ var config = {
       type: 'default',
       options: {
           userName: 'sa', // update me
-          password: 'Paofan@9783' // update me
+          password: '*******' // update me
       }
   },
   options: {
